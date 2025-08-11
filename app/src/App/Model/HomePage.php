4fdc1c998560a4138b7641d63344e27d605b62fe
Lubos Odraska <odraska@slonline.sk>
2025-08-11 16:15:38 +0200

    added slides and featured fonts

--- app/src/App/Model/HomePage.php
+++ app/src/App/Model/HomePage.php
@@ -2,13 +2,22 @@
 
 namespace SLONline\App\Model;
 
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+use SilverStripe\Forms\GridField\GridFieldDataColumns;
+use SilverStripe\ORM\HasManyList;
 use SilverStripe\SiteConfig\SiteConfig;
+use SLONline\Elefont\Model\FontFamilyPage;
+use SLONline\Elefont\Model\WebsiteBlocks\WebsiteBlock;
+use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
 
 /**
  * Home Page
  *
  * @author    Lubos Odraska <odraska@slonline.sk>
  * @copyright Copyright (c) 2025, SLONline, s.r.o.
+ *
+ * @property HasManyList|Slide Slides
  */
 class HomePage extends \Page
 {
@@ -19,7 +28,13 @@ class HomePage extends \Page
 
     private static array $db = [];
 
-    private static array $has_many = [];
+    private static array $has_many = [
+        'Slides' => Slide::class
+    ];
+
+    private static array $owns = [
+        'Slides',
+    ];
 
     private static array $allowed_children = [];
 
@@ -28,5 +43,23 @@ class HomePage extends \Page
         return SiteConfig::current_site_config()->Title . (SiteConfig::current_site_config()->Tagline ? ' - ' . SiteConfig::current_site_config()->Tagline : '');
     }
 
+    public function featuredFontFamilyPages()
+    {
+        return FontFamilyPage::get()
+            ->filter('IsFeatured', true)
+            ->sort('ReleaseDate', 'DESC');
+    }
 
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $config = GridFieldConfig_RecordEditor::create();;
+        $config->addComponent(new GridFieldSortableRows('SortOrder'));
+
+        $fields->addFieldToTab('Root.Slides',
+            GridField::create('Slides', 'Slides', $this->Slides(), $config));
+
+        return $fields;
+    }
 }
---
+++ app/src/App/Model/Slide.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace SLONline\App\Model;
+
+use SilverStripe\Assets\File;
+use SilverStripe\ORM\DataObject;
+
+/**
+ * Slide Data Object
+ *
+ * Represents a slide in the home page slider.
+ *
+ * @author    Lubos Odraska <odraska@slonline.sk>
+ * @copyright Copyright (c) 2025, SLONline, s.r.o.
+ *
+ * @property string $Title
+ * @property int $SortOrder
+ * @property string $Link
+ * @method  HomePage HomePage
+ * @method  File Image
+ * @method  File Video
+ */
+class Slide extends DataObject
+{
+    private static string $table_name = 'Slides';
+    private static string $singular_name = 'Slide';
+    private static string $plural_name = 'Slides';
+
+    private static array $db = [
+        'Title' => 'Varchar(255)',
+        'SortOrder' => 'Int',
+        'Link' => 'Varchar(255)',
+    ];
+
+    private static array $has_one = [
+        'HomePage' => HomePage::class,
+        'Image' => File::class,
+        'Video' => File::class,
+    ];
+
+    private static array $owns = [
+        'Image',
+        'Video'
+    ];
+
+    private static string $default_sort = "SortOrder ASC";
+
+    public function imageNull(): ?File
+    {
+        if ($this->Image()->exists()) {
+            return $this->Image();
+        }
+
+        return null;
+    }
+
+    public function videoNull(): ?File
+    {
+        if ($this->Video()->exists()) {
+            return $this->Video();
+        }
+
+        return null;
+    }
+
+    public function canView($member = null): bool
+    {
+        return true;
+    }
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName('HomePageID');
+        return $fields;
+    }
+}
