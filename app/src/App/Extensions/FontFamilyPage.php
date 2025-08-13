74ff1f75bf2dba92b8c272092172c2c1d0044037
Lubos Odraska <odraska@slonline.sk>
2025-08-13 11:19:41 +0200

    footer button settings

---
+++ app/src/App/Extensions/FontFamilyPage.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SLONline\App\Extensions;
+
+use SilverStripe\Core\Extension;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\NumericField;
+use SLONline\ColorField\Form\ColorField;
+use SLONline\ColorField\ORM\FieldType\DBColor;
+
+/**
+ * Font Family Page Application Extension
+ *
+ * @author    Lubos Odraska <odraska@slonline.sk>
+ * @copyright Copyright (c) 2025, SLONline, s.r.o.
+ *
+ * @property string $FooterButtonBorderColor
+ * @property string $FooterButtonTextColor
+ * @property string $FooterButtonBackgroundColor
+ * @property int $FooterButtonBorderRadius
+ */
+class FontFamilyPage extends Extension
+{
+    private static array $db = [
+        'FooterButtonBorderColor' => DBColor::class,
+        'FooterButtonTextColor' => DBColor::class,
+        'FooterButtonBackgroundColor' => DBColor::class,
+        'FooterButtonBorderRadius' => 'Int'
+    ];
+
+    public function footerButton(): array
+    {
+        return [
+            'borderColor' => $this->owner->FooterButtonBorderColor,
+            'textColor' => $this->owner->FooterButtonTextColor,
+            'backgroundColor' => $this->owner->FooterButtonBackgroundColor,
+            'borderRadius' => $this->owner->FooterButtonBorderRadius
+        ];
+    }
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldsToTab('Root.FooterButton', [
+            ColorField::create('FooterButtonBorderColor', 'Border Color'),
+            NumericField::create('FooterButtonBorderRadius', 'Border Radius'),
+            ColorField::create('FooterButtonTextColor', 'Text Color'),
+            ColorField::create('FooterButtonBackgroundColor', 'Background Color'),
+        ]);
+    }
+}
